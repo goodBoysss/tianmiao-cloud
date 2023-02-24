65008f7cf570487a603249ff7103a7b7e8e1a7cb
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-02-24 15:18:48 +0800

    通过别名获取当前应用渠道包使用情况

--- src/App/Channel/General/ChannelGeneralClient.php
+++ src/App/Channel/General/ChannelGeneralClient.php
@@ -37,17 +37,18 @@ class ChannelGeneralClient extends ChannelClient
 
     /**
      * 通过别名获取当前应用渠道包使用情况
-     * @param $appAlias
-     * @params alias 应用别名
-     * @return array|bool ['package_num' => 1] or []
+     * @param $appAliases
+     * @params aliases 应用别名 例如 'qipao,youmi,qianhuan'
+     * @return array|bool 返回示例(正常) {"code":200,"data":[{"app_id":1,"alias":"qipao","package_num":1}],"message":""}
+     * @return array|bool 返回示例(异常-应用不存在) {"code":200,"data":[],"message":""}
      * @throws TianmiaoCloudException
      * @author yangwenjie <yangwenjie@tianmtech.cn>
      * @datetime 2023/2/23 17:49
      */
-    public function getAppPackageUsedNum($appAlias)
+    public function getAppPackageUsedNum($appAliases)
     {
         return $this->request('/api/app/package/used/num', 'get', array(
-            'alias' => $appAlias
+            'aliases' => $appAliases
         ));
     }
 }
